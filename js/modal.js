1f37799b48e09b669bb590133e3e5ca8f64bb505
al-busova <al.imm.2.6d@gmail.com>
2024-02-04 23:00:37 +0200

    styles of modals

--- js/modal.js
+++ js/modal.js
@@ -3,12 +3,17 @@
 		openModalBtn: document.querySelectorAll("[data-modal-open]"),
 		closeModalBtn: document.querySelector("[data-modal-close]"),
 		modal: document.querySelector("[data-modal]"),
+		openModalCatalog: document.querySelector(".apartment-btn"),
+		modalCatalog: document.querySelector(".modal-catalog"),
+		closeModalCatalog: document.querySelector(".btn-modal-catalog"),
 		header: document.querySelector(".header"),
 	};
 
 	refs.openModalBtn[0].addEventListener("click", toggleModal);
 	refs.openModalBtn[1].addEventListener("click", toggleModal);
+	refs.openModalBtn[2].addEventListener("click", toggleModal);
 	refs.closeModalBtn.addEventListener("click", toggleModal);
+	refs.openModalCatalog.addEventListener("click", toggleModalCatalog);
 
 	function toggleModal() {
 		refs.modal.classList.toggle("is-hidden");
@@ -18,5 +23,14 @@
         } else {
            refs.header.style.position = "static"; 
         }
+	}
+	function toggleModalCatalog() {
+		refs.modalCatalog.classList.toggle("is-hidden");
+		document.body.classList.toggle("no-scroll");
+		if (refs.modalCatalog.classList.contains("is-hidden")) {
+			refs.header.style.position = "fixed";
+		} else {
+			refs.header.style.position = "static";
+		}
 	}
 })();
